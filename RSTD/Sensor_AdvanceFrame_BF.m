d531c4170faa6400c96be732533f915f2c3b6304
Shaikh Islam <shaikhalvee@gmail.com>
2026-02-23 00:04:50 -0600

    sensor config flagged before hard change

--- RSTD/Sensor_AdvanceFrame_BF.m
+++ RSTD/Sensor_AdvanceFrame_BF.m
@@ -77,7 +77,15 @@ function  [ErrStatus] = Sensor_AdvanceFrame_BF(ar_device, params, Tx1_Ph_Deg, Tx
     %%%%%%%%%%%%%-Sensor Configuration%%%%%%%%%%%%-
     %% Profile Configuration
     % Syntax:
-    % Int32 ar1.ProfileConfig_mult(UInt16 RadarDeviceId, UInt16 profileId, Single startFreqConst, Single idleTimeConst, Single adcStartTimeConst, Single rampEndTime, UInt32 tx1OutPowerBackoffCode, UInt32 tx2OutPowerBackoffCode, UInt32 tx3OutPowerBackoffCode, UInt16 tx1PhaseShifter, UInt16 tx2PhaseShifter, UInt16 tx3PhaseShifter, Single freqSlopeConst, Single txStartTime, UInt16 numAdcSamples, UInt16 digOutSampleRate, Char hpfCornerFreq1, Char hpfCornerFreq2, Char rxGain) - Profile configuration API which defines chirp profile parameters
+    % Int32 ar1.ProfileConfig_mult(UInt16 RadarDeviceId, 
+    % UInt16 profileId, Single startFreqConst, Single idleTimeConst, 
+    % Single adcStartTimeConst, Single rampEndTime, UInt32 tx1OutPowerBackoffCode, 
+    % UInt32 tx2OutPowerBackoffCode, UInt32 tx3OutPowerBackoffCode, UInt16 tx1PhaseShifter, 
+    % UInt16 tx2PhaseShifter, UInt16 tx3PhaseShifter, Single freqSlopeConst, 
+    % Single txStartTime, UInt16 numAdcSamples, UInt16 digOutSampleRate, 
+    % Char hpfCornerFreq1, Char hpfCornerFreq2, Char rxGain) 
+    % 
+    % - Profile configuration API which defines chirp profile parameters
     % _I_ UInt16	RadarDeviceId	 - Radar Device Id
     % _I_ UInt16	profileId	 - Chirp Profile Id [0 to 3]
     % _I_ Single	startFreqConst	 - Chirp Start Frequency in GHz
@@ -105,18 +113,18 @@ function  [ErrStatus] = Sensor_AdvanceFrame_BF(ar_device, params, Tx1_Ph_Deg, Tx
     % Tells the device about chirp start frequency, idle time, ADC start time, ramp end, etc.
     Lua_String = sprintf('ar1.ProfileConfig_mult(%d, %d, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %d, %d, 2, 1, %d)', ...
         RadarDeviceId(ar_device), profileID, params.Start_Freq_GHz, params.Idle_Time_us, params.Adc_Start_Time_us, ...
-        params.Ramp_End_Time_us, 0, 0, 0, 0, 0,0, ...
+        params.Ramp_End_Time_us, 0, 0, 0, 0, 0, 0, ...
         params.Slope_MHzperus, params.Tx_Start_Time_us, params.Samples_per_Chirp,...
         params.Sampling_Rate_ksps, params.Rx_Gain_dB);
     
     % Send this Lua command via the RtttNetClientAPI to configure the device
     ErrStatus = RtttNetClientAPI.RtttNetClient.SendCommand(Lua_String);
     disp('HPF cutoff increased to 350 and 700');
-    if (ErrStatus ==30000)
-        disp('Profile Configuration successful for BF')
+    if (ErrStatus == 30000)
+        disp('Profile Configuration successful for BF');
     else
-        disp('Profile Configuration failed for BF')
-        return;
+        disp('Profile Configuration failed for BF');
+        error("Profile Configuration failed: dev=%d lua_string=%d status=%d", ar_device, Lua_String, ErrStatus);
     end
 
 
@@ -157,12 +165,12 @@ function  [ErrStatus] = Sensor_AdvanceFrame_BF(ar_device, params, Tx1_Ph_Deg, Tx
             ChirpCfg, ChirpCfg,profileID, Tx1_Enable(ar_device), Tx2_Enable(ar_device), Tx3_Enable(ar_device));
     
         ErrStatus   =   RtttNetClientAPI.RtttNetClient.SendCommand(Lua_String);
-        %     if (ErrStatus ==30000)
-        %         disp('Chirp config successful')
-        %     else
-        %         disp('Chirp config failed')
-        %         return;
-        %     end
+            if (ErrStatus ==30000)
+                disp('Chirp config successful')
+            else
+                disp('Chirp config failed')
+                return;
+            end
     
         % Set the per-chirp phase shifter values for TX1, TX2, TX3
         % Lua_String = [];
@@ -170,12 +178,12 @@ function  [ErrStatus] = Sensor_AdvanceFrame_BF(ar_device, params, Tx1_Ph_Deg, Tx
             ChirpCfg, ChirpCfg, Tx1_Ph_Deg(cnt, ar_device), Tx2_Ph_Deg(cnt,ar_device), Tx3_Ph_Deg(cnt,ar_device));
     
         ErrStatus   =   RtttNetClientAPI.RtttNetClient.SendCommand(Lua_String);
-        %     if (ErrStatus ==30000)
-        %         disp('Phase shifter per Chirp config successful')
-        %     else
-        %         disp('Phase shifter per Chirp config failed')
-        %         return;
-        %     end
+            if (ErrStatus ==30000)
+                disp('Phase shifter per Chirp config successful')
+            else
+                disp('Phase shifter per Chirp config failed')
+                return;
+            end
     
         cnt = cnt + 1; % move to next steering angle index
 
@@ -195,9 +203,67 @@ function  [ErrStatus] = Sensor_AdvanceFrame_BF(ar_device, params, Tx1_Ph_Deg, Tx
     % We instruct the device how to set up subframes, bursts, loops, etc. for this advanced frame,
     % referencing the parameters in 'params'.
 
+    % Syntax:
+    % Int32 ar1.AdvanceFrameConfig_mult(UInt16 RadarDeviceId, UInt32 NumOfSubFrames, UInt16 ForceProfile, UInt16 SF1ForceProfileIdx, UInt16 SF1ChirpStartIdx, UInt16 SF1NumOfChirps, UInt16 SF1NumOfLoops, UInt32 SF1BurstPeriodicity, UInt16 SF1ChirpStartIdxOffset, UInt16 SF1NumOfBurst, UInt16 SF1NumOfBurstLoops, UInt32 SF1SubFramePeriodicity, UInt16 SF2ForceProfileIdx, UInt16 SF2ChirpStartIdx, UInt16 SF2NumOfChirps, UInt16 SF2NumOfLoops, UInt32 SF2BurstPeriodicity, UInt16 SF2ChirpStartIdxOffset, UInt16 SF2NumOfBurst, UInt16 SF2NumOfBurstLoops, UInt32 SF2SubFramePeriodicity, UInt16 SF3ForceProfileIdx, UInt16 SF3ChirpStartIdx, UInt16 SF3NumOfChirps, UInt16 SF3NumOfLoops, UInt32 SF3BurstPeriodicity, UInt16 SF3ChirpStartIdxOffset, UInt16 SF3NumOfBurst, UInt16 SF3NumOfBurstLoops, UInt32 SF3SubFramePeriodicity, UInt16 SF4ForceProfileIdx, UInt16 SF4ChirpStartIdx, UInt16 SF4NumOfChirps, UInt16 SF4NumOfLoops, UInt32 SF4BurstPeriodicity, UInt16 SF4ChirpStartIdxOffset, UInt16 SF4NumOfBurst, UInt16 SF4NumOfBurstLoops, UInt32 SF4SubFramePeriodicity, UInt16 NumOfFrames, UInt16 TriggerSelect, UInt32 FrameTrigDelay, Byte NumOfCloneSubFrames, UInt32 SF1TotalChirps, UInt16 SF1NumOfAdcSamples, Byte SF1NumOfChirpsInDataPacket, UInt32 SF2TotalChirps, UInt16 SF2NumOfAdcSamples, Byte SF2NumOfChirpsInDataPacket, UInt32 SF3TotalChirps, UInt16 SF3NumOfAdcSamples, Byte SF3NumOfChirpsInDataPacket, UInt32 SF4TotalChirps, UInt16 SF4NumOfAdcSamples, Byte SF4NumOfChirpsInDataPacket) - AdvanceFrameConfig API Defines advanced frame configuration
+	% _I_ UInt16	RadarDeviceId	 	    - Radar Device Id
+	% _I_ UInt32	NumOfSubFrames	 	    - Number Of SubFrames enabled in this frame(b0:15) + SW Sub-Frame triggermode(b0:31
+	% _I_ UInt16	ForceProfile	 	    - Profile is used when that chirp is transmitted(b0:7) + LoopBackCfg(LoopBackCfg(b7)+SubFrameID (b8:9)))(b7:15)
+	% _I_ UInt16	SF1ForceProfileIdx	    - SF1 Force Profile Idx
+	% _I_ UInt16	SF1ChirpStartIdx	    - SF1 Start index of the first chirp in this sub frame
+	% _I_ UInt16	SF1NumOfChirps	 	    - SF1 Number Of unique Chirps per burst
+	% _I_ UInt16	SF1NumOfLoops	 	    - SF1 Number Of times to loop through the unique chirps in each burst, without gaps, using HW
+	% _I_ UInt32	SF1BurstPeriodicity	    - SF1 Minimum time needed for triggering next burst
+	% _I_ UInt16	SF1ChirpStartIdxOffset	- SF1 Chirps Start Idex Offset
+	% _I_ UInt16	SF1NumOfBurst	 	    - SF1 Number Of Bursts constituting this sub frame
+	% _I_ UInt16	SF1NumOfBurstLoops	 	- SF1 Number Of Burst Loops
+	% _I_ UInt32	SF1SubFramePeriodicity	- SF1 SubFrame Periodicity
+	% _I_ UInt16	SF2ForceProfileIdx	 	- SF2 Force Profile Idx
+	% _I_ UInt16	SF2ChirpStartIdx	 	- SF2 Chirp StartIdx
+	% _I_ UInt16	SF2NumOfChirps	 	    - SF2 Num Of Chirps
+	% _I_ UInt16	SF2NumOfLoops	 	    - SF2 Number of times to loop over the set of above defined bursts for this subframe
+	% _I_ UInt32	SF2BurstPeriodicity	 	- SF2BurstPeriodicity
+	% _I_ UInt16	SF2ChirpStartIdxOffset	- SF2 Chirp Start Idx Offset
+	% _I_ UInt16	SF2NumOfBurst	 	    - SF2 Num Of Burst
+	% _I_ UInt16	SF2NumOfBurstLoops	 	- SF2 Num Of BurstLoops
+	% _I_ UInt32	SF2SubFramePeriodicity	- SF2 SubFrame Periodicity
+	% _I_ UInt16	SF3ForceProfileIdx	 	- SF3 Force Profile Idx
+	% _I_ UInt16	SF3ChirpStartIdx	 	- SF3 Chirp StartIdx
+	% _I_ UInt16	SF3NumOfChirps	 	    - SF3 Num Of Chirps
+	% _I_ UInt16	SF3NumOfLoops	 	    - SF3 Num Of Loops
+	% _I_ UInt32	SF3BurstPeriodicity	 	- SF3 Burst Periodicity
+	% _I_ UInt16	SF3ChirpStartIdxOffset	- SF3 Chirp Start Idx Offset
+	% _I_ UInt16	SF3NumOfBurst	 	    - SF3 Num Of Burst
+	% _I_ UInt16	SF3NumOfBurstLoops	 	- SF3 Num Of BurstLoops
+	% _I_ UInt32	SF3SubFramePeriodicity	- SF3 SubFrame Periodicity
+	% _I_ UInt16	SF4ForceProfileIdx	 	- SF4 Force Profile Idx
+	% _I_ UInt16	SF4ChirpStartIdx	 	- SF4 Chirp StartIdx
+	% _I_ UInt16	SF4NumOfChirps	 	    - SF4 Num Of Chirps
+	% _I_ UInt16	SF4NumOfLoops	 	    - SF4 Num Of Loops
+	% _I_ UInt32	SF4BurstPeriodicity	 	- SF4 Burst Periodicity
+	% _I_ UInt16	SF4ChirpStartIdxOffset	- SF4 Chirp Start Idx Offset
+	% _I_ UInt16	SF4NumOfBurst	 	    - SF 4Num Of Burst
+	% _I_ UInt16	SF4NumOfBurstLoops	 	- SF4 Num Of BurstLoops
+	% _I_ UInt32	SF4SubFramePeriodicity	- SF4 SubFrame Periodicity
+	% _I_ UInt16	NumOfFrames	 	        - Number Of Frames to transmit
+	% _I_ UInt16	TriggerSelect	 	    - Trigger Select such as software and hardware trigger 
+	% _I_ UInt32	FrameTrigDelay	 	    - Optional time delay from the SYNC-IN trigger to the occurance of frame chirps
+	% _I_ Byte	    NumOfCloneSubFrames	 	- Num Of Clone SubFrames
+	% _I_ UInt32	SF1TotalChirps	 	    - SF1 Total Chirps
+	% _I_ UInt16	SF1NumOfAdcSamples	 	- SF1 Num Of AdcSamples
+	% _I_ Byte	SF1NumOfChirpsInDataPacket	- SF1 Num Of Chirps In DataPacket
+	% _I_ UInt32	SF2TotalChirps	 	    - SF2 Total Chirps
+	% _I_ UInt16	SF2NumOfAdcSamples	 	- SF2 Num Of AdcSamples
+	% _I_ Byte	SF2NumOfChirpsInDataPacket	- SF2 Num Of Chirps In DataPacket
+	% _I_ UInt32	SF3TotalChirps	 	    - SF3 Total Chirps
+	% _I_ UInt16	SF3NumOfAdcSamples	 	- SF3 Num Of AdcSamples
+	% _I_ Byte	SF3NumOfChirpsInDataPacket	- SF3Num Of Chirps In DataPacket
+	% _I_ UInt32	SF4TotalChirps	 	    - SF4 Total Chirps
+	% _I_ UInt16	SF4NumOfAdcSamples	 	- SF4 Num Of AdcSamples
+	% _I_ Byte	SF4NumOfChirpsInDataPacket	- SF4 Num Of Chirps In DataPacket
+
     numSubFrames = params.numSubFrames;
 
-    %paramters for subframe 1
+    % paramters for subframe 1
 
     SF1ChirpStartIdx = params.SF1ChirpStartIdx;
     SF1NumChirps = params.SF1NumChirps;
